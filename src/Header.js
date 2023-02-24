6f9ba902d349ccc319c8fcc3e3d0d6ce24c53070
Dan Hung <dan891221@gmail.com>
2023-02-24 12:17:19 +0800

    Logout and keep login in

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,25 @@
-
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
-import ChatIcon from '@material-ui/icons/Chat';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import HeaderOption from './HeaderOption';
-
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import HeaderOption from "./HeaderOption";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
 
 function Header() {
+  const dispatch = useDispatch();
+
+  const logoutApp = () => {
+    dispatch(logout());
+  };
+
   return (
-    <div className='header'>
-
+    <div className="header">
       <div className="header__left">
-
         <img
           src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
           alt="linkedin"
@@ -25,7 +29,6 @@ function Header() {
           <SearchIcon />
           <input placeholder="Search" type="text" />
         </div>
-
       </div>
 
       <div className="header__right">
@@ -35,12 +38,13 @@ function Header() {
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
         <HeaderOption
+          onClick={logoutApp}
           avatar="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
           title="Me"
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
